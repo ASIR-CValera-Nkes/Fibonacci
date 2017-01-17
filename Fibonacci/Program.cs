75d6de2628b2cb42489abe9e267706014ba7126c
Alvaro <ikillnukes1@gmail.com>
2017-01-17 20:36:48 +0100

    Añadido un contador de capacidad y el tiempo requerido para la operación

--- Fibonacci/Program.cs
+++ Fibonacci/Program.cs
@@ -5,6 +5,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Numerics;
+using System.Globalization;
+using System.Diagnostics;
 
 namespace Fibonacci
 {
@@ -13,37 +15,39 @@ namespace Fibonacci
         private static int posicion;
         static void Main(string[] args)
         {
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
             Console.Write("Escribe la posición que quieres calcular de la serie de Fibonacci: ");
             string p = Console.ReadLine();
             if(!string.IsNullOrWhiteSpace(p) && int.TryParse(p, out posicion) && posicion > 1)
             {
                 BigRational r = 0,
-                           lr = 1;
+                            lr = 1;
                     //j = 1;
                 Console.WriteLine();
-                //Console.WriteLine("Pos #1) 0 + 0 = 0");
-                for (int i = 0; i < posicion; ++i)
+                Console.WriteLine("En la posición 1 tenemos que el primer número es 0 y el segundo número es 1 y como resultado da 1, seguimos sumando:");
+                for (int i = 0; i < posicion - 1; ++i)
                 {
-                    //if(i > 0)
-                        Console.WriteLine("Pos #" + (i + 1) + ") " + r + " + " + lr + " = " + (lr + r));
+                    Console.WriteLine("Pos #" + (i + 2) + ") " + r.Numerator + " + " + lr.Numerator + " = " + (lr + r).Numerator);
                     BigRational v = lr;
                     lr = r + lr;
                     r = v;
                 }
+                sw.Stop();
                 Console.WriteLine();
-                Console.WriteLine("El resultado es {0}, Fhi = {1:F5}", lr, lr / r);
+                Console.WriteLine("El resultado es {0}, la consulta ha tardado {1} ms.", lr.Numerator.FormatString(), sw.ElapsedMilliseconds); //, (lr / r).Numerator
                 Console.WriteLine();
                 Console.WriteLine("Comparando capacidades:");
                 Console.WriteLine();
-                Console.WriteLine("{0:F5}% de un short (2^15 = {1})", lr.GetWholePart() * 100 / ushort.MaxValue, ushort.MaxValue);
-                Console.WriteLine("{0:F5}% de un ushort (2^16 = {1})", lr.GetWholePart() * 100 / short.MaxValue, short.MaxValue);
-                Console.WriteLine("{0:F5}% de un int (2^31 = {1})", lr.GetWholePart() * 100 / int.MaxValue, int.MaxValue);
-                Console.WriteLine("{0:F5}% de un uint (2^32 = {1})", lr.GetWholePart() * 100 / uint.MaxValue, uint.MaxValue);
-                Console.WriteLine("{0:F5}% de un long (2^63 = {1})", lr.GetWholePart() * 100 / long.MaxValue, long.MaxValue);
-                Console.WriteLine("{0:F5}% de un ulong (2^64 = {1})", lr.GetWholePart() * 100 / ulong.MaxValue, ulong.MaxValue);
-                Console.WriteLine("0,{0:F5}% de un float ({1})", (lr * 100 / float.MaxValue).GetFractionPart().ToString().Substring(2, 25), float.MaxValue.ToString());
-                Console.WriteLine("0,{0:F5}% de un double ({1})", (lr * 100 / double.MaxValue).GetFractionPart().ToString().Substring(2, 25), double.MaxValue.ToString());
-                Console.WriteLine("0,{0:F5}% de un decimal ({1})", (lr * 100 / decimal.MaxValue).GetFractionPart().ToString().Substring(2, 25), decimal.MaxValue.ToString());
+                Console.WriteLine("{0:F5}% de un short (2^15 = {1})", (lr.GetWholePart() * 100 / ushort.MaxValue).FormatString(), ushort.MaxValue);
+                Console.WriteLine("{0:F5}% de un ushort (2^16 = {1})", (lr.GetWholePart() * 100 / short.MaxValue).FormatString(), short.MaxValue);
+                Console.WriteLine("{0:F5}% de un int (2^31 = {1})", (lr.GetWholePart() * 100 / int.MaxValue).FormatString(), int.MaxValue);
+                Console.WriteLine("{0:F5}% de un uint (2^32 = {1})", (lr.GetWholePart() * 100 / uint.MaxValue).FormatString(), uint.MaxValue);
+                Console.WriteLine("{0:F5}% de un long (2^63 = {1})", (lr.GetWholePart() * 100 / long.MaxValue).FormatString(), long.MaxValue);
+                Console.WriteLine("{0:F5}% de un ulong (2^64 = {1})", (lr.GetWholePart() * 100 / ulong.MaxValue).FormatString(), ulong.MaxValue);
+                Console.WriteLine("{0}% de un float ({1})", (lr * 100 / float.MaxValue).FormatString(), float.MaxValue.ToString());
+                Console.WriteLine("{0}% de un double ({1})", (lr * 100 / double.MaxValue).FormatString(), double.MaxValue.ToString());
+                Console.WriteLine("{0}% de un decimal ({1})", (lr * 100 / decimal.MaxValue).FormatString(), decimal.MaxValue.ToString());
             }
             else
             {
@@ -52,6 +56,85 @@ namespace Fibonacci
                 Main(null);
             }
             Console.Read();
+        }
+    }
+    public static class BigRationalExtensions
+    {
+        public static string ToDecimalString(this BigRational r, int precision)
+        {
+            var fraction = r.GetFractionPart();
+
+            // Case where the rational number is a whole number
+            if (fraction.Numerator == 0 && fraction.Denominator == 1)
+            {
+                return r.GetWholePart() + ".0";
+            }
+
+            var adjustedNumerator = (fraction.Numerator
+                                               * BigInteger.Pow(10, precision));
+            var decimalPlaces = adjustedNumerator / fraction.Denominator;
+
+            // Case where precision wasn't large enough.
+            if (decimalPlaces == 0)
+            {
+                return "0.0";
+            }
+
+            // Give it the capacity for around what we should need for 
+            // the whole part and total precision
+            // (this is kinda sloppy, but does the trick)
+            var sb = new StringBuilder(precision + r.ToString().Length);
+
+            bool noMoreTrailingZeros = false;
+            for (int i = precision; i > 0; i--)
+            {
+                if (!noMoreTrailingZeros)
+                {
+                    if ((decimalPlaces % 10) == 0)
+                    {
+                        decimalPlaces = decimalPlaces / 10;
+                        continue;
+                    }
+
+                    noMoreTrailingZeros = true;
+                }
+
+                // Add the right most decimal to the string
+                sb.Insert(0, decimalPlaces % 10);
+                decimalPlaces = decimalPlaces / 10;
+            }
+
+            // Insert the whole part and decimal
+            sb.Insert(0, ".");
+            sb.Insert(0, r.GetWholePart());
+
+            return sb.ToString();
+        }
+        public static string FormatString(this BigRational br)
+        {
+            string r = br.ToDecimalString(25);
+            if(br.Denominator.GetExponent() > 25 || br.Numerator.GetExponent() > 25)
+            {
+                int e = GetExponent(br.Numerator) - GetExponent(br.Denominator);
+                r = string.Format("~1E{0}", e);
+            }
+            return r;
+        }
+        public static string FormatString(this BigInteger bi)
+        {
+            int e = GetExponent(bi);
+            string r = bi.ToString();
+            if (e > 25)
+                r = string.Format("{0}.{1}E+{2}", r.Substring(0, 1), r.Substring(1, 10), e);
+            return r;
+        }
+        public static int GetExponent(this BigRational br)
+        {
+            return GetExponent(br.Numerator);
+        }
+        public static int GetExponent(this BigInteger bi)
+        {
+            return bi.ToString().Length - 1;
         }
     }
 }
